116270209ecc64fc630baa2b6aa7f944a8f47c41
harshbones <davranjar@hotmail.com>
2018-10-24 18:25:23 +0200

    ajout exercice

---
+++ README.md
@@ -0,0 +1,5 @@
+# README
+
+Projet réalisé au sein de l'équipe comprenant Valérian Michelot, Paul Guérin, François Flous, William Horel, David Rangeard
+
+Une fois le dossier cloné, vous pouvez faire un : bundle install puis rails db:reset afin de vider le contenu et lancer une nouvelle génération de seed.
---
+++ db/seeds.rb
@@ -0,0 +1,23 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+6.times do
+  user = User.create!(name: Faker::Superhero.name)
+end
+
+20.times do
+  post = Post.create!(user_id: Random.new.rand(1..User.count), title: Faker::HowIMetYourMother.quote)
+end
+
+15.times do
+  comment = Comment.create!(post_id: Random.new.rand(1..Post.count), user_id: Random.new.rand(1..User.count), content: Faker::Lovecraft.sentence, rank: 1)
+end
+
+15.times do
+  answer = Answer.create!(comment_id: Random.new.rand(1..Comment.count), user_id: Random.new.rand(1..User.count), content: Faker::Lovecraft.sentence, rank: 2)
+end
